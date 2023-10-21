6a001665a1468c2cf89d6d7baac4bf9e4f1aa142
phutaekwondo <lainguyenvinhphu@gmail.com>
2023-10-21 16:59:31 +0700

    set the steps size of the ground by the ground scripts

--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -1,5 +1,14 @@
+using System.Numerics;
 using UnityEngine;
 
 public class Ground : MonoBehaviour
 {
+    [SerializeField] private Material groundMaterial;
+    [SerializeField] int widthSize = 5;
+    [SerializeField] int heightSize = 5;
+    private void Awake() 
+    {
+        this.groundMaterial.SetFloat("_HorizontalSteps", this.widthSize);
+        this.groundMaterial.SetFloat("_VerticalSteps", this.heightSize);
+    }
 }
