a6e0eb6cd17eed16f21f4c3c63d92c0083f13e13
phutaekwondo <lainguyenvinhphu@gmail.com>
2023-10-29 14:55:50 +0700

    rename to CellTransformGetter

---
+++ Assets/Scripts/Level/CellTransformGetter.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CellTransformGetter : MonoBehaviour
+{
+    static public CellTransformGetter Instance;
+
+    [SerializeField] private Ground ground;
+    
+    private void Awake() 
+    {
+        CellTransformGetter.Instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    public Vector3 GetCellPosition(CellOrdinate cellOrdinate) 
+    {
+        return this.ground.GetCellPosition(cellOrdinate);
+    }
+
+    public Vector3 GetCellSize() 
+    {
+        return this.ground.GetCellSize();
+    }
+}
