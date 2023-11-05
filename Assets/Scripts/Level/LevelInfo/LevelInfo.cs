b4618d8b67fdf5bc0b4eecefb65aeb0b5b3d0b1f
phutaekwondo <lainguyenvinhphu@gmail.com>
2023-11-05 16:43:23 +0700

    prevent modify level info

--- Assets/Scripts/Level/LevelInfo/LevelInfo.cs
+++ Assets/Scripts/Level/LevelInfo/LevelInfo.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewLevel", menuName = "Level/New Level")]
 public class LevelInfo : ScriptableObject
 {
-    public uint groundSize;
-    public Vector2Int playerStartPosition;
-    public Vector2Int enemyStartPosition;
-    public List<Vector2Int> walls;
+    public uint groundSize {get; private set;}
+    public Vector2Int playerStartPosition {get; private set;}
+    public Vector2Int enemyStartPosition {get; private set;}
+    public List<Vector2Int> walls {get; private set;}
 }
