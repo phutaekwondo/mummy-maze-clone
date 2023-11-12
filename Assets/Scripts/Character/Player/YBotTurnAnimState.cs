96b638df6715f0bdd5a109afb53fa51791bcaf80
phutaekwondo <lainguyenvinhphu@gmail.com>
2023-11-12 16:59:36 +0700

    implement YbotTurnAnimState

---
+++ Assets/Scripts/Character/Player/YBotTurnAnimState.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public delegate void OnEnterTransition(AnimatorTransitionInfo transitionInfo, ETurnType turnType);
+
+public class YBotTurnAnimState : StateMachineBehaviour
+{
+    public static event OnEnterTransition onEnterTransition;
+    [SerializeField] private ETurnType turnType;
+    private bool isEnteredTransition = false;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+        this.isEnteredTransition = false;
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateUpdate(animator, stateInfo, layerIndex);
+
+        if (animator.IsInTransition(layerIndex) && !this.isEnteredTransition) 
+        {
+            this.isEnteredTransition = true;
+            YBotTurnAnimState.onEnterTransition.Invoke(animator.GetAnimatorTransitionInfo(layerIndex), this.turnType);
+        }
+    }
+}
