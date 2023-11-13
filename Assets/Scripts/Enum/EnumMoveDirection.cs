6239fe05cfc96012a114f61e1279cc84ad8ddf07
phutaekwondo <lainguyenvinhphu@gmail.com>
2023-11-13 21:52:39 +0700

    implement TurnMoveDirection method

--- Assets/Scripts/Enum/EnumMoveDirection.cs
+++ Assets/Scripts/Enum/EnumMoveDirection.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 public enum EnumMoveDirection 
 {
     None,
@@ -5,4 +6,48 @@ public enum EnumMoveDirection
     Left,
     Right,
     Down
+}
+
+public class EnumMoveDirectionHelper {
+    public static EnumMoveDirection TurnMoveDirection(EnumMoveDirection direction, ETurnType turnType) 
+    {
+        if (direction == EnumMoveDirection.None){
+            return EnumMoveDirection.None;
+        }
+
+        List<EnumMoveDirection> orderedMoveDirection = new List<EnumMoveDirection> {
+            EnumMoveDirection.Up,
+            EnumMoveDirection.Right,
+            EnumMoveDirection.Down,
+            EnumMoveDirection.Left,
+        };
+
+        int moveDirIndex = orderedMoveDirection.IndexOf(direction);
+
+        switch(turnType) 
+        {
+            case ETurnType.Left:
+                moveDirIndex -= 1;
+                break;
+            case ETurnType.Right:
+                moveDirIndex += 1;
+                break;
+            case ETurnType.Back:
+                moveDirIndex += 2;
+                break;
+            default:
+                break;
+        }
+
+        if (moveDirIndex < 0)
+        {
+            moveDirIndex += orderedMoveDirection.Count;
+        }
+        else if (moveDirIndex >= orderedMoveDirection.Count) 
+        {
+            moveDirIndex %= orderedMoveDirection.Count;
+        }
+
+        return orderedMoveDirection[moveDirIndex];
+    }
 }
