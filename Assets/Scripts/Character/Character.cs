6c458a3d58b1cc1f27aa887264c05d80ceb7ce5d
phutaekwondo <lainguyenvinhphu@gmail.com>
2023-12-16 15:54:04 +0700

    get controllers for character

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -7,6 +7,18 @@ abstract public class Character : MonoBehaviour
     private CharacterTransformController characterTransformController;
     private CellOrdinate cellOrdinate;
 
+    private void Awake() 
+    {
+        this.characterAnimController = this.GetComponent<CharacterAnimController>();
+        if (this.characterAnimController == null){
+            this.characterAnimController = this.AddComponent<CharacterAnimController>();
+        }
+        this.characterTransformController = this.GetComponent<CharacterTransformController>();
+        if (this.characterTransformController == null){
+            this.characterTransformController = this.AddComponent<CharacterTransformController>();
+        }
+    }
+
     public void SetCellOrdinate(CellOrdinate cellOrdinate)
     {
         this.cellOrdinate = cellOrdinate;
