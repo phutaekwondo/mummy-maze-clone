2ce3765ed9472adbc6aedf98dbd006028bf9f4a5
phutaekwondo <lainguyenvinhphu@gmail.com>
2023-12-16 16:13:45 +0700

    implement transformController::TweenToCell

--- Assets/Scripts/Character/CharacterTransformController.cs
+++ Assets/Scripts/Character/CharacterTransformController.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DigitalRuby.Tween;
 using UnityEngine;
 
 public class CharacterTransformController : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 2f;
+
     public void SetPosition(CellOrdinate cellOrdinate)
     {
         Vector3 position = CellTransformGetter.Instance.GetCellPosition(cellOrdinate);
@@ -13,5 +17,35 @@ public class CharacterTransformController : MonoBehaviour
     public void SetEulerAngles(EnumMoveDirection direction)
     {
         this.gameObject.transform.forward = EnumMoveDirectionHelper.GetVec3Direction(direction);
+    }
+
+    public void TweenToCell(CellOrdinate cellOrdinate, Action onComplete = null)
+    {
+        Action<ITween<Vector3>> updatePosition = (v) =>
+        {
+            this.gameObject.gameObject.transform.position = v.CurrentValue;
+        };
+
+        Action<ITween<Vector3>> onTweenComplete = (v) => 
+        {
+            if (onComplete != null)
+            {
+                onComplete();
+            }
+        };
+
+        Vector3 position = CellTransformGetter.Instance.GetCellPosition(cellOrdinate);
+
+        float duration = (this.gameObject.transform.position - position).magnitude / this.moveSpeed;
+
+        this.gameObject.gameObject.Tween(
+            this.gameObject.ToString() + "TweenPositionToward",
+            this.gameObject.transform.position,
+            position,
+            duration,
+            TweenScaleFunctions.Linear,
+            updatePosition,
+            onTweenComplete
+        );
     }
 }
