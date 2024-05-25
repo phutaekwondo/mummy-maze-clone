19dc675aa6b3e378ac69bfdf8e340ea98e97dba4
phutaekwondo <lainguyenvinhphu@gmail.com>
2024-05-25 16:24:19 +0700

    [level editor] log chosen levelInfo when accept button is clicked

--- Assets/Scripts/LevelEditor/CreateNewLevel/LoadLevelManager.cs
+++ Assets/Scripts/LevelEditor/CreateNewLevel/LoadLevelManager.cs
@@ -1,14 +1,53 @@
+using System.IO;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoadLevelManager : MonoBehaviour
 {
+    [SerializeField] private TMP_Text levelInfoPathText;
+    [SerializeField] Button acceptButton;
+    private string levelInfoPath;
+
+    private void Update()
+    {
+        this.acceptButton.interactable = this.IsLevelInfoPathValid(this.levelInfoPath);
+    }
+
+    private bool IsLevelInfoPathValid(string levelInfoPath)
+    {
+        if (levelInfoPath == null || levelInfoPath.Length == 0)
+        {
+            return false;
+        }
+
+        //check if file exists
+        return File.Exists(levelInfoPath);
+    }
+
+    public void OnAcceptButtonClicked()
+    {
+        string relative = FileUtil.GetProjectRelativePath(this.levelInfoPath);
+        LevelInfo levelInfo = AssetDatabase.LoadAssetAtPath<LevelInfo>(relative);
+        if (levelInfo == null)
+        {
+            Debug.LogError("LevelInfo not found at path: " + relative);
+            return;
+        }
+        else
+        {
+            Debug.Log(levelInfo);
+        }
+    }
+
     public void OnBrowseButtonClicked()
     {
         string path = EditorUtility.OpenFilePanel("Load Level", "", "asset");
         if (path.Length != 0)
         {
-            Debug.Log("Loading level from: " + path);
+            levelInfoPathText.text = path;
+            this.levelInfoPath = path;
         }
     }
 }
