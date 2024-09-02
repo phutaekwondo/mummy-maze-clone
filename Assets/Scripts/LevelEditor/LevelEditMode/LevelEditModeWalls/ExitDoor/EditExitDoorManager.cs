0d049d2903a7e898e3edddb1bc1cfe0bec831017
phutaekwondo <lainguyenvinhphu@gmail.com>
2024-09-02 14:32:27 +0700

    set enable by mouse event

--- Assets/Scripts/LevelEditor/LevelEditMode/LevelEditModeWalls/ExitDoor/EditExitDoorManager.cs
+++ Assets/Scripts/LevelEditor/LevelEditMode/LevelEditModeWalls/ExitDoor/EditExitDoorManager.cs
@@ -8,16 +8,31 @@ public class EditExitDoorManager : MonoBehaviour
     private void Awake()
     {
         this.exitDoorTargetsPanel = this.GetComponent<ExitDoorTargetsPanel>();
-        this.exitDoorTargetsPanel.SetEnabled(true);
+        this.exitDoorTargetsPanel.SetEnabled(false);
         this.exitDoorTargetsPanel.handleTargetMouseEnter = this.HandleTargetMouseEnter;
     }
 
     public void ReceiveSpawnedExitDoor(ExitDoor levelEditorExitDoor)
     {
         this.levelEditorExitDoor = levelEditorExitDoor;
+        ExitDoorMouseHandler exitDoorMouseHandler = this.levelEditorExitDoor.GetComponent<ExitDoorMouseHandler>();
+        exitDoorMouseHandler.onMouseDown = this.HandleExitDoorMouseDown;
+        exitDoorMouseHandler.onMouseUp = this.HandleExitDoorMouseUp;
     }
 
-    public void HandleTargetMouseEnter(BlockedCell blockedCell)
+    private void HandleExitDoorMouseDown()
+    {
+        Debug.Log("Exit door mouse down");
+        this.exitDoorTargetsPanel.SetEnabled(true);
+    }
+
+    private void HandleExitDoorMouseUp()
+    {
+        Debug.Log("Exit door mouse up");
+        this.exitDoorTargetsPanel.SetEnabled(false);
+    }
+
+    private void HandleTargetMouseEnter(BlockedCell blockedCell)
     {
         this.levelEditorExitDoor.SetWall(blockedCell);
     }
