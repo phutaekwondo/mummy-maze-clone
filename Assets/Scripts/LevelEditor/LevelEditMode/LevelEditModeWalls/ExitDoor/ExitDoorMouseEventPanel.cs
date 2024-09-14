090eef7136d88da714459b7e47aaac5dfc5b08b8
Lai Nguyen Vinh Phu <lainguyenvinhphu@gmail.com>
2024-09-14 13:03:26 +0700

    change name to mouse event panel

--- Assets/Scripts/LevelEditor/LevelEditMode/LevelEditModeWalls/ExitDoor/ExitDoorMouseEventPanel.cs
+++ Assets/Scripts/LevelEditor/LevelEditMode/LevelEditModeWalls/ExitDoor/ExitDoorMouseEventPanel.cs
@@ -3,10 +3,23 @@ using UnityEngine;
 
 namespace LevelEditor
 {
-    public class ExitDoorMouseHandler : MonoBehaviour
+    public class ExitDoorMouseEventPanel : MonoBehaviour
     {
         public Action onMouseDown;
         public Action onMouseUp;
+        public Action onMouseEnter;
+        public Action onMouseExit;
+
+        private void OnMouseEnter()
+        {
+            this.onMouseEnter?.Invoke();
+        }
+
+        private void OnMouseExit()
+        {
+            this.onMouseExit?.Invoke();
+        }
+
         private void OnMouseDown()
         {
             this.onMouseDown?.Invoke();
