d88e9bd77f36a44477a056f969b9fa02da7cd1ab
Lai Nguyen Vinh Phu <lainguyenvinhphu@gmail.com>
2024-10-19 17:29:05 +0700

    set default values in LevelData

--- Assets/Scripts/Level/Data/LevelData.cs
+++ Assets/Scripts/Level/Data/LevelData.cs
@@ -2,9 +2,9 @@ using System.Collections.Generic;
 
 public class LevelData
 {
-    public int groundSize;
-    public CellOrdinate playerStartPosition;
-    public CellOrdinate enemyStartPosition;
-    public List<BlockedCell> walls;
-    public BlockedCell exitDoor;
+    public int groundSize = 0;
+    public CellOrdinate playerStartPosition = new CellOrdinate(0, 0);
+    public CellOrdinate enemyStartPosition = new CellOrdinate(0, 0);
+    public List<BlockedCell> walls = new List<BlockedCell>();
+    public BlockedCell exitDoor = new BlockedCell(new CellOrdinate(0, 0), new CellOrdinate(-1, 0));
 }
